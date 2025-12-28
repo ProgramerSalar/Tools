33f850d3043d7984c0dc1365a0524107e94884e3
ProgramerSalar <manishkumar60708090@gmail.com>
2025-12-28 18:50:15 +0530

    update

--- extract_latent/script/script.sh
+++ extract_latent/script/script.sh
@@ -1,5 +1,5 @@
 GPUS=1
-ANNO_FILE=/content/Tools/annotation/video_data_files_path.jsonl
+ANNO_FILE=/content/Tools/annotation/video_text_json.json
 VAE_MODEL_PATH=/content/Tools/extract_latent/PATH/vae_model_ckpt
 WIDTH=640
 HEIGHT=384
